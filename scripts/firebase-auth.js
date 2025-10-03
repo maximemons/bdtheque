8282022550ce5b72d4aab5d047c01fab06b95a28
Maxime Mons <pro.maxime.mons@gmail.com>
2025-10-03 15:24:01 +0200

    tmp

--- scripts/firebase-auth.js
+++ scripts/firebase-auth.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getAuth, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyB74A20CTGGkH-UCQzBofLDx7iBMy3LzV0",
